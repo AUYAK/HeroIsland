45a51ffbd7d24b96d89dcc9e096c5ba9244174df
Alex Y <blablabla@somemail.com>
2023-07-26 10:26:01 +0300

    Equipment manager is added. Added functionality to apply equipment on the hero.

---
+++ HeroIsland/Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipSlot;
+    public int armorModifier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+        //Equip use
+        //Equip remove from the slot
+    }
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.Remove(this);
+
+    }
+
+}
+
+public enum EquipmentSlot {Head, Chest, Legs, Weapon, Shield, Feet}
